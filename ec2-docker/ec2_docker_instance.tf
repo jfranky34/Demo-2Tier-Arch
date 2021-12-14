275c239bda652f0b990342a2364326bae6f16989
jfranky34 <jfranky34@gmail.com>
2021-12-14 01:53:24 -0500

    update-ec2-instance

--- ec2-docker/ec2_docker_instance.tf
+++ ec2-docker/ec2_docker_instance.tf
@@ -52,7 +52,24 @@ resource "aws_instance" "ec2-docker-instance" {
   security_groups = [aws_security_group.ec2_docker_sg.id]
   key_name = data.aws_key_pair.ec2_docker_key.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_rds_profile.name
-  user_data = file("../ec2-docker/docker_install.sh")
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+
+    ## Install Docker ##
+    sudo apt-get update
+    sudo apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+      stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+    sudo usermod -aG docker ubuntu
+    sudo chmod 666 /var/run/docker.sock
+    sudo apt-get install -y wget
+  EOF
 
   root_block_device {
     volume_type = "gp2"
