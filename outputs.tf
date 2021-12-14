4069a2b19284f95ba9c012f6eff9563e51cd1da7
jfranky34 <jfranky34@gmail.com>
2021-12-14 10:58:10 -0500

    update output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 
-output "rds_db_endpoint" {
-  description = "The connection endpoint"
-  value = module.db.db_instance_endpoint
-}
+# output "rds_db_endpoint" {
+#   description = "The connection endpoint"
+#   value = module.db.db_instance_endpoint
+# }
 
 output "instance_id" {
   description = "The EC2 Instance Id"
