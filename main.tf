3ca42dddbcfe4ea408c8449af399879a781baf3b
jfranky34 <jfranky34@gmail.com>
2021-12-14 01:30:42 -0500

    update-main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "fmdemo-ec2-docker" {
     AWS_REGION  = var.AWS_REGION
     vpc_private_subnet = module.fmdemo-vpc.private_subnet_id
     vpc_id = module.fmdemo-vpc.fmdemo_vpc_id
-    vpc_public_subnet1 = module.fmdemo-vpc.public_subnet_id
+    vpc_public_subnet = module.fmdemo-vpc.public_subnet_id
 }
 
 #Define Provider
